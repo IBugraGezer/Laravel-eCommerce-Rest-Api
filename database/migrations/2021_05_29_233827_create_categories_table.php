51573cc60eea50c62e06d25d830fe3e6da7fd803
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-05-30 02:44:15 +0300

    categories migration updated

--- database/migrations/2021_05_29_233827_create_categories_table.php
+++ database/migrations/2021_05_29_233827_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string("name",50);
-            $table->boolean("active");
+            $table->string("name",50)->unique();
+            $table->boolean("active")->default(0);
             $table->timestamps();
         });
     }
