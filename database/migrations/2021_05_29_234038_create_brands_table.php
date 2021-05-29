d58ae702d7287101f25c1ed268627ef598daad9a
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-05-30 02:47:53 +0300

    brands migration updated

--- database/migrations/2021_05_29_234038_create_brands_table.php
+++ database/migrations/2021_05_29_234038_create_brands_table.php
@@ -15,7 +15,8 @@ class CreateBrandsTable extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('name', )
+            $table->string('name', 100)->unique();
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
