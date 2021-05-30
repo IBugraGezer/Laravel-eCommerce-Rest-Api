c8d24a8d3e2573c35a0531b593bc30e0e4cbb2ef
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-05-30 19:59:58 +0300

    order tracks migration updated

--- database/migrations/2021_05_30_155738_create_product_images_table.php
+++ database/migrations/2021_05_30_155738_create_product_images_table.php
@@ -15,9 +15,18 @@ class CreateProductImagesTable extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
-
+            $table->unsignedBigInteger('product_id');
+            $table->integer('place_number');
             $table->timestamps();
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products');
+
+            $table->unique(['product_id', 'place_number']);
         });
+
+
     }
 
     /**
