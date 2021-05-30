4d66c3f5ee2a3d945bc6d288a2879702c11e3e3d
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-05-30 21:29:02 +0300

    foreigns defined in basket items migration

--- database/migrations/2021_05_30_170326_create_basket_items_table.php
+++ database/migrations/2021_05_30_170326_create_basket_items_table.php
@@ -19,6 +19,15 @@ class CreateBasketItemsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->integer('count');
             $table->timestamps();
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
         });
     }
 
