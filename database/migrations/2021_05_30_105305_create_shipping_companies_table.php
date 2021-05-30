b3f24ff406c2c42433afbf2060e2f648abf6a754
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-05-30 13:54:46 +0300

    shipping companies migration updated

--- database/migrations/2021_05_30_105305_create_shipping_companies_table.php
+++ database/migrations/2021_05_30_105305_create_shipping_companies_table.php
@@ -15,6 +15,9 @@ class CreateShippingCompaniesTable extends Migration
     {
         Schema::create('shipping_companies', function (Blueprint $table) {
             $table->id();
+            $table->string('company_name', 100);
+            $table->string('tracking_link', 1000);
+            $table->boolean('active');
             $table->timestamps();
         });
     }
