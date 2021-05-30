cd5307c371d454062919be808ecb1894865fbea4
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-05-30 13:50:43 +0300

    orders migration updated

--- database/migrations/2021_05_30_103100_create_orders_table.php
+++ database/migrations/2021_05_30_103100_create_orders_table.php
@@ -15,7 +15,31 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('status');
+            $table->string('delivery_address_id',400);
+            $table->string('billing_address_id', 400);
+            $table->unsignedFloat('total_price', 7, 2);
+            $table->string('ship_tracking_code', 100)->nullable();
+            $table->unsignedBigInteger('shipping_company_id')->nullable();
+            $table->string('order_code', 100);
+            $table->boolean('active')->default(1);
             $table->timestamps();
+
+            $table->foreign('status')
+                ->references('id')
+                ->on('order_statuses');
+
+            $table->foreign('delivery_address_id')
+                ->references('id')
+                ->on('addresses');
+
+            $table->foreign('billing_address_id')
+                ->references('id')
+                ->on('addresses');
+
+            $table->foreign('shipping_company_id')
+                ->references('id')
+                ->on('shipping_companies');
         });
     }
 
