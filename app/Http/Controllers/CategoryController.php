f0a0a1acb4dfda979073f6b3242a3b4c94840919
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-03 02:10:40 +0300

    category routes completed

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin_check', ['except' => ['index','show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -13,51 +19,83 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            return response(Category::all(),200);
+        } catch (\Exception $e) {
+            return response(["message" => "An error occured."],500);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        return response(["message" => "CategoryController store method"]);
+        $data = $request->validate([
+            'name' => 'required|string|min:2|max:40',
+            'active' => 'numeric|min:0|max:1'
+        ]);
+        try {
+            $category = Category::create($data);
+            return response($category, 200);
+        } catch (\Exception $e) {
+            return response(["message" => "An error occured.", 500]);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        try {
+            return response(Category::findOrFail($id), 200);
+        } catch (\Exception $e) {
+            return response(["message" => "An error occured"], 500);
+        }
+
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->validate([
+            'name' => 'string|min:2|max:40',
+            'active' => 'numeric|min:0|max:1'
+        ]);
+        try {
+            $category = Category::findOrFail($id);
+            $category->update($data);
+            return response($category, 200);
+        } catch (\Exception $e) {
+            return response(["message" => "An error occured"], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        try {
+            return response(Category::destroy($id), 200);
+        } catch (\Exception $e) {
+            return response(["message" => "An error occured"], 500);
+        }
     }
 }
