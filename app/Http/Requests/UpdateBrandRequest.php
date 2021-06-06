3cbd1deb14e00ed1ff02b91b6aaae29451f16bbc
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-06 19:49:14 +0300

    brand request classes created

---
+++ app/Http/Requests/UpdateBrandRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBrandRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:2|max:50|unique:brands,name',
+            'active' => 'required|numeric|min:0|max:1'
+        ];
+    }
+}
