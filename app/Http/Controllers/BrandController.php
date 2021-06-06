627d1f8fe690466b0a3191243e0f709cbf2c8907
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-06 20:03:10 +0300

    brand and category authorizations edited

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -12,7 +12,7 @@ class BrandController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth_check')->except(['index', 'show']);
+        $this->middleware('auth_check')->except(['index']);
     }
 
     /**
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth_check')->except(['index', 'show']);
+        $this->middleware('auth_check')->except(['index']);
     }
 
     /**
