48f65e05aefe7202b0f34b720c3d281f57f8b5f4
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-08 00:13:00 +0300

    product store request class updated

--- app/Http/Requests/ProductStoreRequest.php
+++ app/Http/Requests/ProductStoreRequest.php
@@ -24,14 +24,14 @@ class ProductStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'category_id' => 'required|exists:categories',
-            'brand_id' => 'required|exists:brands',
+            'category_id' => 'required|exists:categories,id',
+            'brand_id' => 'required|exists:brands,id',
             'name' => 'required|string|min:3|max:450',
-            'price' => 'required|numeric|min:0.01|max:9999999.99',
+            'price' => 'required|numeric|regex:/^\d+(\.\d{7,2})?$/',
             'slug' => 'string|min:10|max:500|unique:products,slug',
-            'stock' => 'numeric|min:0|max:999999999',
+            'stock' => 'int|min:0|max:999999999',
             'description' => 'string|min:10|max:1400',
-            'active' => 'numeric|min:0|max:1'
+            'active' => 'int|min:0|max:1'
         ];
     }
 }
