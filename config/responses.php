7c99f74148447e39cf5e0be632785d56323ddf55
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-08 08:39:54 +0300

    response structure changed

--- config/responses.php
+++ config/responses.php
@@ -1,11 +1,21 @@
 <?php
 
-return [
-
+$messages = [
     'error' => 'An error occured.',
-
     'unauthorized' => 'You are not authorized for this action.',
-
-    'not_found' => 'not found'
-
+    'not_found' => 'Not found.'
 ];
+
+$responses = [
+    'as_array' => []
+];
+
+$responses = array_merge($responses, $messages);
+
+$errorMessagesAsArray = [];
+
+foreach($messages as $key => $message) {
+    $responses['as_array'][$key] = ["message" => $message];
+}
+
+return $responses;
