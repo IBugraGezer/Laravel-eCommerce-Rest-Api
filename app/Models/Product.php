63e77a038a5d1d897cd87d8e6893b4b98db06825
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-08 13:25:57 +0300

    product model updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,8 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['serial_number', 'rating_average', 'id'];
+
     public function category() {
         return $this->belongsTo(Category::class);
     }
