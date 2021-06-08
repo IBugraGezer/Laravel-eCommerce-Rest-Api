9bbb3024f75bd6b08a26966a4f7293c2ea2018df
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-08 22:04:48 +0300

    product store request class updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductStoreRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -34,7 +35,7 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductStoreRequest $request)
     {
         if(auth('sanctum')->user()->cannot('create', Product::class))
             return response(config('responses.as_array.unauthorized'), 403);
