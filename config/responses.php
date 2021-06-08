476c9dfad46ed6843d10fff05e9bbf7c4d16429f
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-08 09:05:11 +0300

    responses updated

--- config/responses.php
+++ config/responses.php
@@ -1,9 +1,12 @@
 <?php
 
 $messages = [
-    'error' => 'An error occured.',
-    'unauthorized' => 'You are not authorized for this action.',
-    'not_found' => 'Not found.'
+    'error' => "An error occured.",
+    'unauthorized' => "You are not authorized for this action.",
+    'not_found' => "Not found.",
+    'already_logged_in' => "You are already logged in.",
+    'bad_creds' => "Bad creds.",
+    'logged_out' => "Logged out."
 ];
 
 $responses = [
