cd5f4cd879e1905d95c6352dbcda9987e93476a5
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-09 08:28:14 +0300

    product serial code system updated

--- app/Helper/HelperFunctions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function generateProductSerialNumber() {
-    $chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $serialNumber = substr(str_shuffle($chars), 0, 8);
-    return $serialNumber;
-}
