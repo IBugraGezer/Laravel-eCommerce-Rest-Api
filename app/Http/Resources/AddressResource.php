016fa369f89946e0492f995ff0c5f6edf3d912ea
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-09 11:20:07 +0300

    address resource updated

--- app/Http/Resources/AddressResource.php
+++ app/Http/Resources/AddressResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Helper\AuthHelper;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AddressResource extends JsonResource
@@ -14,6 +15,11 @@ class AddressResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return array_filter([
+            'id' => AuthHelper::checkAdmin() ? $this->id : null,
+            'user_id' => AuthHelper::checkAdmin() ? $this->user_id : null,
+            'address_name' => $this->address_name,
+            'address' => $this->address,
+        ]);
     }
 }
