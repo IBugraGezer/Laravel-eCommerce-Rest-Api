ebd50e8e3f64b8de6ad63bee01f94705e68657d0
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-12 15:27:08 +0300

    file controller updated

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\FileManager;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+    public function getPublicDir() {
+        return FileManager::getPublicDir();
+    }
+
+    public function getAnyItem(GetAnyItemRequest $request) {
+        $data = $request->validated();
+
+        return response(FileManager::getAnyDir($data->path), 200);
+    }
 }
