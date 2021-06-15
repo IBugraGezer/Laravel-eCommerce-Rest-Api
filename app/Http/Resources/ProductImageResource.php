87d4213793470f28fbab47a63cfc61d158d9f969
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-15 10:50:04 +0300

    product image resourcee updated

--- app/Http/Resources/ProductImageResource.php
+++ app/Http/Resources/ProductImageResource.php
@@ -18,9 +18,11 @@ class ProductImageResource extends JsonResource
         return array_filter([
             'id' => AuthHelper::checkAdmin() ? $this->id : null,
             'product_id' => AuthHelper::checkAdmin() ? $this->product_id : null,
-            'image_path' => $this->image_path,
+            'image_path' => $this->path,
             'place_number' => $this->place_number,
-            'is_cover' => $this->is_cover
-        ]);
+            'is_cover' => $this->is_cover == 1
+        ], function($value) {
+            return ($value !== null && $value !== '');
+        });
     }
 }
